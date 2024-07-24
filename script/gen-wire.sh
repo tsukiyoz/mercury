fa629001e7d179d765e7b72fc8d0692796d27bce
lazywoo <tsukiyo6@163.com>
2024-07-24 17:56:41 +0800

    docs: gen-wire.sh improve

--- script/gen-wire.sh
+++ script/gen-wire.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
-ROOT=$(cd `dirname $0`/..; pwd)
+ROOT=$(cd "$(dirname "$0")/.." || exit ; pwd)
 
-services=" user sms interactive comment oauth2 crontask article captcha ranking bff follow "
+services=("user" "sms" "interactive" "comment" "oauth2" "crontask" "article" "captcha" "ranking" "bff" "follow")
 
+echo "=======> check services..."
 args=$@
 if [ $# -gt 0 ]; then
-  tmpSrv=" "
-  for target in ${args}
-  do
-    if [[ ${services} == *" $target "* ]]; then
-      tmpSrv+="$target "
+  tmpSrv=()
+  for target in "${args[@]}"; do
+    found=false
+    for service in "${services[@]}"; do
+        if [[ $service == "$target" ]]; then
+          tmpSrv+=("$service")
+          found=true
+          break
+        fi
+    done
+    if ! $found; then
+      echo "=======> service $target does not exist, ignore..."
     fi
   done
-  services=$tmpSrv
+  services=("${tmpSrv[@]}")
 fi
 
-echo "=======> wire service [${services}]"
+if [ ${#services[@]} -eq 0 ]; then
+  echo "=======> no valid services specified, exiting..."
+  exit 0
+fi
 
-for service in ${services}
+echo "=======> wire service" "[${services[*]}]"
+
+for service in "${services[@]}"
 do
-  cd ${ROOT}/${service}
-  wire
+  service_dir="${ROOT}/${service}"
+  if [ -d "$service_dir" ]; then
+    cd "$service_dir" || exit
+    wire
+  else
+    echo "$service_dir does not exist, ignore..."
+  fi
 done
 
-cd ${ROOT}
+cd "${ROOT}" || exit
 echo "=======> finished wire"
